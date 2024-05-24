8abcd66f69430190a704e2eb0526cee51303e18f
iuliastaci <iuliastaci@users.noreply.github.com>
2024-05-24 21:12:21 +0300

    Database setup

---
+++ src/main/db/BdConnection.java
@@ -0,0 +1,23 @@
+package main.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class BdConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/food_delivery";
+    private static final String USER = "root";
+    private static final String PASSWORD = "Java#2024";
+
+    private BdConnection() {
+    }
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+}
